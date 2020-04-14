f98a9a068deda0f0330ab3b3eb8e03268b05aa0f
Raihan Afroz <you@example.com>
2020-04-14 19:48:12 +0600

    0.1.1.1
    updating success toast

--- rtoasty/src/main/java/com/example/rtoasty/RToastySuccess.java
+++ rtoasty/src/main/java/com/example/rtoasty/RToastySuccess.java
@@ -6,6 +6,12 @@ import es.dmoral.toasty.Toasty;
 
 public class RToastySuccess {
     public RToastySuccess(Context context, String msg) {
-        Toasty.success(context, msg);
+        Toasty.success(context, msg).show();
     }
+    public RToastySuccess(Context context, String msg, int time) {
+        Toasty.success(context, msg, time).show();
+    }
+//    public RToastySuccess(Context context, String msg) {
+//        Toasty.success(context, msg, time).show();
+//    }
 }
